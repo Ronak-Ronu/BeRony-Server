5117cee1948378089ddc56246cb03a3c8edcc133
Ronak-Ronu <ronakronu02@gmail.com>
2024-10-26 20:01:05 +0530

    updated secure.

--- server.js
+++ server.js
@@ -21,21 +21,24 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopol
 app.use('/api', postRoutes);
 app.use('/api', draftRoutes);
 
+
 function sendRequest() {
   axios.get(process.env.API_LIVE_URL)
-      .then(response => {
-          console.log(`Keep-alive request successful with status: ${response.status}`);
-      })
-      .catch(error => {
-          console.error('Error in keep-alive request:', error.message);
-      });
+    .then(response => {
+      console.log(`Keep-alive request successful with status: ${response.status}`);
+    })
+    .catch(error => {
+      console.error('Error in keep-alive request:', error.message);
+      console.error(error.response ? error.response.data : error.message);
+    });
 }
 
 function keepAlive() {
-  setInterval(sendRequest, 12 * 60 * 1000);  
+  setInterval(sendRequest, 60 * 1000);
 }
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
+  keepAlive()
 });
