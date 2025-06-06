ac1a559d319071cc92e4407051db2c037892c0b1
Ronak-Ronu <ronakronu02@gmail.com>
2025-06-06 22:44:05 +0530

    Add more activityType in enum

--- models/UserActivitySchema.js
+++ models/UserActivitySchema.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 
 const userActivitySchema = new mongoose.Schema({
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  activityType: { type: String, enum: ['post', 'edit'], required: true },
+  userId: { type: String, required: true },
+  activityType: { type: String, enum: ['post', 'edit','comment','read','collab','poll','profile-update','plant','story','checkin'], required: true },
   timestamp: { type: Date, required: true },
 });
 
